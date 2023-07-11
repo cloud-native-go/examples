a01e38b7a606a0af7a33c42eb1b424a637eb5d29
Matthew Titmus <mtitmus@yext.com>
2023-07-11 14:51:00 -0400

    Re-running pre-commit hooks

--- ch05/ch05_08/pglogger.go
+++ ch05/ch05_08/pglogger.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Matthew A. Titmus
+ * Copyright 2023 Matthew A. Titmus
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -77,7 +77,7 @@ func (l *PostgresTransactionLogger) Run() {
 			if err != nil {
 				errors <- err
 			}
-			
+
 			l.wg.Done()
 		}
 	}()
